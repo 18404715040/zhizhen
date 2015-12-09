5e792eb80d11023ca5179814ae04a68392224abc
20141105074 <20141105074qf@mac25.local>
2015-12-09 18:36:03 +0800

    hand
    通过指针输出a的地址，和指针p的地址和值

--- zhizhen/main.cpp
+++ zhizhen/main.cpp
@@ -6,10 +6,14 @@
 //  Copyright © 2015年 20141105074. All rights reserved.
 //
 
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]){
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=&x &a=%x &p=%x p=%x *p=%x\n",a,&a,&p,p,*p);
     return 0;
 }
