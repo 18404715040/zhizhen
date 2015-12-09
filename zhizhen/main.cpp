a9e2106c24d7d49e17693624c56fbba8bd6a81d4
20141105074 <20141105074qf@mac25.local>
2015-12-09 18:31:22 +0800

    Initial Commit

---
+++ zhizhen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zhizhen
+//
+//  Created by 20141105074 on 15/12/9.
+//  Copyright © 2015年 20141105074. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
